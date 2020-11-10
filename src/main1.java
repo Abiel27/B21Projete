f20cedaf14a17433f6780915dcb074b4a5082835
Abiel Menghistu <abielmenghistu@gmail.com>
2020-11-10 14:48:25 -0500

    Initial commit, this is a test for education purpose!

--- src/main1.java
+++ src/main1.java
@@ -1,6 +1,6 @@
 public class main1 {
     public static void main(String[] args) {
-        System.out.println("Hello world this is Abiel:-)");
+        System.out.println("Hello world#1 this is Abiel:-)");
     }
 }
 
